2d38b41921838b9eeea6ef261645c4d82d11447c
Ivan Tabera <ivantabera19@gmail.com>
2021-04-05 19:00:57 -0500

    Este es el primer commit del proyecto de persistencia de datos

---
+++ src/main/java/com/stayhome/mensajes_app/Conexion.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.stayhome.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * 
+ * @author ivantabera19 <sguergachi at gmail.com>
+ */
+public class Conexion {
+    
+    public Connection get_connection(){
+        
+        Connection connection = null;
+            
+        try{
+            
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app", "root", "");
+            
+            if(connection != null){
+                System.out.println("Conexion exitosa");
+            }
+            
+        } catch(SQLException e){
+            System.out.println(e);
+        }
+
+        return connection;
+    }
+
+}
