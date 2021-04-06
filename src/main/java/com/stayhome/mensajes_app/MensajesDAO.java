158c2a01d9b8fbc591a73d498c9adea72d2c85c2
Ivan Tabera <ivantabera19@gmail.com>
2021-04-06 13:28:39 -0500

    Se gerera el metodo para insertar la informacion en la base de datos

--- src/main/java/com/stayhome/mensajes_app/MensajesDAO.java
+++ src/main/java/com/stayhome/mensajes_app/MensajesDAO.java
@@ -1,9 +1,39 @@
 package com.stayhome.mensajes_app;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MensajesDAO {
 
     public static void crearMensajeDB(Mensajes mensaje){
 
+        //Se invoca el objeto Conexion
+        Conexion dbConnect = new Conexion();
+
+        //Nos conectamos a la BD
+        try (Connection conexion = dbConnect.get_connection()){
+
+            PreparedStatement ps = null;
+
+            //Insercion de los datos a la BD
+            try{
+
+                String query =  "INSERT INTO mensajes (mensaje, autor_mensaje) VALUES ( ?, ?)";
+                ps = conexion.prepareStatement(query);
+                ps.setString(1, mensaje.getMensaje());
+                ps.setString(2, mensaje.getAutorMensaje());
+                ps.executeUpdate();
+                System.out.println("El mensaje fue creado correctamente");
+
+            } catch (SQLException ex){
+                System.out.println(ex);
+            }
+
+            //Mostramos la excepcion si es que no nos podemos conectar a la BD
+        } catch (SQLException e){
+            System.out.println(e);
+        }
     }
 
     public static void leerMensajesDB(){
--- src/main/java/com/stayhome/mensajes_app/MensajesService.java
+++ src/main/java/com/stayhome/mensajes_app/MensajesService.java
@@ -1,8 +1,24 @@
 package com.stayhome.mensajes_app;
 
+import java.util.Scanner;
+
 public class MensajesService {
 
-    public static void crearMensaje(){}
+    public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Escribe tu mensaje");
+        String mensaje = sc.nextLine();
+
+        System.out.println("Tu nombre");
+        String nombre = sc.nextLine();
+
+        Mensajes registro = new Mensajes();
+        registro.setMensaje(mensaje);
+        registro.setAutorMensaje(nombre);
+
+        MensajesDAO.crearMensajeDB(registro);
+    }
 
     public static void listarMensajes(){}
 
